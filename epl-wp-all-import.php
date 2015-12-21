75ba4476c4eca2b263ef1f8285d717eb963d8b08
Merv Barrett <mervb1@gmail.com>
2015-12-22 01:15:13 +0800

    Force Easy Property Listings core to load first on activation of importer

--- epl-wp-all-import.php
+++ epl-wp-all-import.php
@@ -76,7 +76,7 @@ if ( ! class_exists( 'EPL_WP_All_Import_Add_On' ) ) :
 		private function hooks() {
 			// activation
 			add_action( 'admin_init', array( $this, 'activation' ) );
-            add_action("activated_plugin", array($this, "epl_wpallimport_load_last") );
+			add_action( 'activated_plugin', array($this, 'epl_wpallimport_load_epl_core_first' ) );
 		}
 
 		/**
@@ -162,18 +162,25 @@ if ( ! class_exists( 'EPL_WP_All_Import_Add_On' ) ) :
 			return (!empty($do_not_update)) ? false : true;
 		}
 
-        function epl_wpallimport_load_last() {
-            $this_plugin = str_replace( WP_PLUGIN_DIR . '/', '', __FILE__ );
-            $active_plugins = get_option('active_plugins');
-            $this_plugin_key = array_search($this_plugin, $active_plugins);
-            if ($this_plugin_key) { // if it's 0 it's the first plugin already, no need to continue
-                array_splice($active_plugins, $this_plugin_key, 1);
-                $active_plugins[] = $this_plugin;
-                update_option('active_plugins', $active_plugins);
-            }
-        }
-
-
+		/*
+		 * Force Easy Property Listings to load first on activation
+		 *
+		 * @access private
+		 * @since 1.0.4
+		 * @return void
+		 */
+		function epl_wpallimport_load_epl_core_first() {
+			$epl_core_path = 'easy-property-listings/easy-property-listings.php';
+			if ( is_plugin_active( $epl_core_path ) ) {
+				if ( $plugins = get_option( 'active_plugins' ) ) {
+					if ( $key = array_search( $epl_core_path, $plugins ) ) {
+						array_splice( $plugins, $key, 1 );
+						array_unshift( $plugins, $epl_core_path );
+						update_option( 'active_plugins', $plugins );
+					}
+				}
+			}
+		}
 	}
 endif; // End if class_exists check
 /*
