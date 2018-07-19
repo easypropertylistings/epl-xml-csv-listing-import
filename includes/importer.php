b3feae1551979704025459025cf346957f5b56c0
Merv Barrett <mervb1@gmail.com>
2018-07-19 08:53:19 +0800

    Fix: Bolding log messages corrected

--- includes/importer.php
+++ includes/importer.php
@@ -293,7 +293,7 @@ function epl_wpimport_delete_images($default,$post_object,$xml_object) {
 				current($xml_object['objects']['img'][0]['modTime']);
 		}
 	}
-	
+
 
 	$new_mod_date = strtotime(epl_feedsync_format_date($new_mod_date));
 
@@ -372,7 +372,7 @@ function epl_wpimport_is_post_to_update_depricated( $pid , $xml_node) {
 
 	global $epl_wpimport;
 	//add_action('pmxi_before_post_import', 'epl_wpimport_post_saved_notification', 10, 1);
-	$epl_wpimport->log( '<b>deprecated version running<b>' );
+	$epl_wpimport->log( '<b>deprecated version running</b>' );
 	$live_import	=	function_exists('epl_get_option')  ?  epl_get_option('epl_wpimport_skip_update') : 'off';
 	if ( $live_import == 'on' && get_post_meta($pid,'property_mod_date',true) != '' ) {
 		/** only update posts if new data is available **/
@@ -405,7 +405,7 @@ function epl_wpimport_is_post_to_update( $continue_import,$pid , $xml_node,$impo
 
 	global $epl_wpimport;
 	//add_action('pmxi_before_post_import', 'epl_wpimport_post_saved_notification', 10, 1);
-	$epl_wpimport->log( '<b>latest version running<b>' );
+	$epl_wpimport->log( '<b>latest version running</b>' );
 
 	$live_import	=	function_exists('epl_get_option')  ?  epl_get_option('epl_wpimport_skip_update') : 'off';
 	if ( $live_import == 'on' && get_post_meta($pid,'property_mod_date',true) != '' ) {
